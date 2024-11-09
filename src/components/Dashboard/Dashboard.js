7486882fc1700f95390cc902fe8496dd409647de
Vivek Srivastava <viveksrivastava6180@gmail.com>
2024-11-09 14:30:35 +0530

    feat: Add Arrow Navigation on tabs 🔥

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,21 +1,51 @@
 "use client";
 
 import { Tabs } from "antd";
-import { useMemo } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { Typography } from "antd";
 import TicketsTable from "../TicketsTable/TicketsTable";
 
 const { Title } = Typography;
 
-const tabsType = ['Open', 'In Progress', 'Closed'];
+const tabsType = ["Open", "In Progress", "Closed"];
 
 const items = tabsType.map((tab, index) => ({
-    label: <div className="font-medium">{tab}</div>,
-    key: String(index + 1),
-  }));
-;
+  label: <div className="font-medium">{tab}</div>,
+  key: String(index + 1),
+}));
 
 export default function Dashboard() {
+  const [selectedTab, setSelectedTab] = useState("1");
+
+  const handleTabChange = (key) => {
+    console.log(key);
+    setSelectedTab(key);
+  };
+
+  useEffect(() => {
+    const handleKeyDown = (e) => {
+      const tabKeys = tabsType.map((_, index) => String(index + 1));
+      if (tabKeys.includes(e.key)) {
+        setSelectedTab(e.key);
+      } else if (e.key === "ArrowLeft") {
+        setSelectedTab((prevTab) => {
+          const newTabIndex = parseInt(prevTab) - 1;
+          return newTabIndex >= 1 ? String(newTabIndex) : prevTab;
+        });
+      } else if (e.key === "ArrowRight") {
+        setSelectedTab((prevTab) => {
+          const newTabIndex = parseInt(prevTab) + 1;
+          return newTabIndex <= tabsType.length ? String(newTabIndex) : prevTab;
+        });
+      }
+    };
+    const keydownHandler = (e) => handleKeyDown(e);
+
+    window.addEventListener("keydown", keydownHandler);
+    return () => {
+      window.removeEventListener("keydown", keydownHandler);
+    };
+  }, [selectedTab]);
 
   return (
     <div className="w-screen flex justify-center">
@@ -25,14 +55,16 @@ export default function Dashboard() {
           <Title level={5}>View and manage your tickets</Title>
         </div>
         <Tabs
-          defaultActiveKey="1"
+          defaultActiveKey={"1"}
           type="line"
           size={"small"}
           items={items}
+          activeKey={selectedTab}
           animated={{ inkBar: true, tabPane: true }}
           className="mt-8"
+          onChange={handleTabChange}
         />
-        <TicketsTable />
+        <TicketsTable selectedTab={tabsType[selectedTab-1]} />
       </div>
     </div>
   );
